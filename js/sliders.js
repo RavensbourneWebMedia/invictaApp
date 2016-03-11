35d8ea4167c36c9030a77e83b6d6b556b0e72a01
Matteo Menapace <hello@baddeo.com>
2016-03-11 16:35:18 +0000

    JS

---
+++ js/sliders.js
@@ -0,0 +1,33 @@
+// see https://github.com/kenwheeler/slick/
+$(document).ready( function() 
+{ 
+    $('.receptionSlider').slick(
+    {
+        autoplay: true, 
+        lazyLoad: 'ondemand',
+        fade: true,
+        cssEase: 'linear',
+        speed: 500 // in milliseconds
+    })
+    
+    $('#year1AudioBook').slick(
+    {
+        lazyLoad: 'ondemand',
+        fade: true,
+        cssEase: 'linear',
+        speed: 200 // in milliseconds
+    })
+    
+    // stop playing audios when moving from slide to slide
+    $('#year1AudioBook').on('beforeChange', function(event, slick, currentSlide, nextSlide)
+    {
+      var $audios = $( 'audio' )
+      $audios.each(function(index, audio)
+      {
+        // console.log(audio)
+        audio.pause()
+      })
+    })
+})
+
+// read the manual at http://kenwheeler.github.io/slick/
